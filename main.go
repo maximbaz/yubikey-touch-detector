ba512f94d49e7f6de80ff9796f842b4aac262f6b
Maxim Baz <git@maximbaz.com>
2017-11-04 15:05:40 +0100

    Prototype watching GPG touch request

--- main.go
+++ main.go
@@ -2,10 +2,55 @@ package main
 
 import (
 	"os"
+	"os/exec"
+	"time"
 
 	"github.com/rjeczalik/notify"
 	log "github.com/sirupsen/logrus"
 )
+
+func checkGPGCardStatus() *exec.Cmd {
+	cmd := exec.Command("gpg", "--no-tty", "--card-status")
+	if err := cmd.Start(); err != nil {
+		log.Error(err)
+	}
+	return cmd
+}
+
+func checkGPG() {
+	for i := 0; i < 20; i++ {
+		cmd := checkGPGCardStatus()
+		timer := time.AfterFunc(100*time.Millisecond, func() {
+			cmd.Process.Kill()
+		})
+		err := cmd.Wait()
+		timer.Stop()
+
+		if err != nil {
+			log.Debug("GPG START")
+			checkGPGCardStatus().Wait()
+			log.Debug("GPG STOP")
+			break
+		}
+	}
+}
+
+func watchGPG() {
+	// We are only interested in the first event, should skip all subsequent ones
+	events := make(chan notify.EventInfo)
+	file := os.ExpandEnv("$HOME/.gnupg/pubring.kbx")
+	if err := notify.Watch(file, events, notify.InOpen); err != nil {
+		log.Fatal(err)
+	}
+	defer notify.Stop(events)
+
+	for {
+		select {
+		case <-events:
+			checkGPG()
+		}
+	}
+}
 
 func watchU2F() {
 	// It's important to not miss a single event, so have a small buffer
@@ -30,6 +75,7 @@ func main() {
 	log.SetLevel(log.DebugLevel)
 
 	go watchU2F()
+	go watchGPG()
 
 	wait := make(chan bool)
 	<-wait

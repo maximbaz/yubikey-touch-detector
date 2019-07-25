8d00b2562dad461387de2131debbe45827001736
Mikhail Klementev <root@dumpstack.io>
2019-07-25 06:17:48 +0000

    Implement desktop notifications using libnotify (#4)

---
+++ notifier/libnotify.go
@@ -0,0 +1,26 @@
+package notifier
+
+import (
+	"os/exec"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func notifySend(text string) error {
+	return exec.Command("notify-send", text).Run()
+}
+
+// SetupLibnotifyNotifier configures a notifier to show all touch requests with libnotify
+func SetupLibnotifyNotifier(notifiers map[string]chan Message) {
+	touch := make(chan Message, 10)
+	notifiers["notifier/libnotify"] = touch
+
+	for {
+		if <-touch == GPG_ON {
+			err := notifySend("Yubikey is waiting for a touch")
+			if err != nil {
+				log.Error("Cannot send desktop notification: ", err)
+			}
+		}
+	}
+}

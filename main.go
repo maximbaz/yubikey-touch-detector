ae60dbc0e4b7a09a9da25c3671a083b89319812c
Maxim Baz <git@maximbaz.com>
2019-07-25 09:06:10 +0200

    Add --version

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"path"
@@ -12,18 +13,27 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const appVersion = "1.3.0"
+
 func main() {
 	defaultGpgPubringPath := "$GNUPGHOME/pubring.kbx or $HOME/.gnupg/pubring.kbx"
 
+	var version bool
 	var verbose bool
 	var libnotify bool
 	var u2fAuthPendingPath string
 	var gpgPubringPath string
 	flag.BoolVar(&verbose, "v", false, "print verbose output")
+	flag.BoolVar(&version, "version", false, "print version and exit")
 	flag.BoolVar(&libnotify, "libnotify", false, "show desktop notifications using libnotify")
 	flag.StringVar(&u2fAuthPendingPath, "u2f-authpending-path", "/var/run/user/1000/pam-u2f-authpending", "path to pam-u2f-authpending file")
 	flag.StringVar(&gpgPubringPath, "gpg-pubring-path", defaultGpgPubringPath, "path to gpg's pubring.kbx file")
 	flag.Parse()
+
+	if version {
+		fmt.Println("YubiKey touch detector version:", appVersion)
+		os.Exit(0)
+	}
 
 	if verbose {
 		log.SetLevel(log.DebugLevel)

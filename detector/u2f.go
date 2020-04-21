0e9f683751744b300c135909269938f98db71975
Maxim Baz <git@maximbaz.com>
2020-04-21 16:35:46 +0200

    Document used constants, fix U2F blinking on YubiKey 4

--- detector/u2f.go
+++ detector/u2f.go
@@ -15,6 +15,39 @@ import (
 	"github.com/vtolstov/go-ioctl"
 )
 
+const (
+	// https://fidoalliance.org/specs/u2f-specs-master/inc/u2f_hid.h
+	// and its backwards-compatible successor
+	// https://fidoalliance.org/specs/fido2/fido-client-to-authenticator-protocol-v2.1-rd-20191217.html
+	TYPE_INIT          = 0x80
+	CTAPHID_MSG        = TYPE_INIT | 0x03
+	CTAPHID_KEEPALIVE  = TYPE_INIT | 0x3b
+	FIDO_USAGE_PAGE    = 0xf1d0
+	FIDO_USAGE_CTAPHID = 0x01
+	STATUS_UPNEEDED    = 0x02
+
+	// https://fidoalliance.org/specs/u2f-specs-master/inc/u2f.h
+	U2F_SW_CONDITIONS_NOT_SATISFIED = 0x6985
+
+	// https://github.com/torvalds/linux/blob/master/include/linux/hid.h
+	HID_ITEM_TYPE_GLOBAL           = 1
+	HID_ITEM_TYPE_LOCAL            = 2
+	HID_GLOBAL_ITEM_TAG_USAGE_PAGE = 0
+	HID_LOCAL_ITEM_TAG_USAGE       = 0
+)
+
+var (
+	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/hidraw.h
+	HIDIOCGRDESCSIZE = ioctl.IOR('H', 1, 4)
+	HIDIOCGRDESC     = ioctl.IOR('H', 2, unsafe.Sizeof(hidrawDescriptor{}))
+)
+
+// https://github.com/torvalds/linux/blob/master/include/uapi/linux/hidraw.h
+type hidrawDescriptor struct {
+	Size  uint32
+	Value [4096]uint8
+}
+
 // WatchU2F watches when YubiKey is waiting for a touch on a U2F request
 func WatchU2F(notifiers *sync.Map) {
 	checkAndInitWatcher := func(devicePath string) {
@@ -44,11 +77,6 @@ func WatchU2F(notifiers *sync.Map) {
 	}
 }
 
-type hidrawDescriptor struct {
-	Size  uint32
-	Value [4096]uint8
-}
-
 func isFidoU2FDevice(devicePath string) bool {
 	if !strings.HasPrefix(devicePath, "/dev/hidraw") {
 		return false
@@ -61,14 +89,14 @@ func isFidoU2FDevice(devicePath string) bool {
 	defer device.Close()
 
 	var size uint32
-	err = ioctl.IOCTL(device.Fd(), ioctl.IOR('H', 1, 4), uintptr(unsafe.Pointer(&size)))
+	err = ioctl.IOCTL(device.Fd(), HIDIOCGRDESCSIZE, uintptr(unsafe.Pointer(&size)))
 	if err != nil {
 		log.Warnf("Cannot get descriptor size for device '%v': %v", devicePath, err)
 		return false
 	}
 
 	data := hidrawDescriptor{Size: size}
-	err = ioctl.IOCTL(device.Fd(), ioctl.IOR('H', 2, unsafe.Sizeof(data)), uintptr(unsafe.Pointer(&data)))
+	err = ioctl.IOCTL(device.Fd(), HIDIOCGRDESC, uintptr(unsafe.Pointer(&data)))
 	if err != nil {
 		log.Warnf("Cannot get descriptor for device '%v': %v", devicePath, err)
 		return false
@@ -82,9 +110,12 @@ func isFidoU2FDevice(devicePath string) bool {
 		typ := (prefix & 0b00001100) >> 2
 		size := prefix & 0b00000011
 
-		if typ == 1 && tag == 0 && data.Value[i+1] == 0xd0 && data.Value[i+2] == 0xf1 {
+		val1b := data.Value[i+1]
+		val2b := int(data.Value[i+1]) | (int(data.Value[i+2]) << 8)
+
+		if typ == HID_ITEM_TYPE_GLOBAL && tag == HID_GLOBAL_ITEM_TAG_USAGE_PAGE && val2b == FIDO_USAGE_PAGE {
 			isFido = true
-		} else if typ == 2 && tag == 0 && data.Value[i+1] == 0x01 {
+		} else if typ == HID_ITEM_TYPE_LOCAL && tag == HID_LOCAL_ITEM_TAG_USAGE && val1b == FIDO_USAGE_CTAPHID {
 			hasU2F = true
 		}
 
@@ -108,9 +139,13 @@ func runU2FWatcher(devicePath string, notifiers *sync.Map) {
 
 	payload := make([]byte, 64)
 	lastMessage := notifier.U2F_OFF
+	var u2fOffTimer *time.Timer
 	for {
 		_, err = device.Read(payload)
 		if err != nil {
+			if u2fOffTimer != nil {
+				u2fOffTimer.Stop()
+			}
 			notifiers.Range(func(k, v interface{}) bool {
 				v.(chan notifier.Message) <- notifier.U2F_OFF
 				return true
@@ -118,21 +153,44 @@ func runU2FWatcher(devicePath string, notifiers *sync.Map) {
 			return
 		}
 
-		isU2F := payload[4] == 0x83 && payload[7] == 0x69 && payload[8] == 0x85
-		isFIDO2 := payload[4] == 0xbb && payload[7] == 0x02
+		val1b := payload[7]
+		val2b := (int(payload[7]) << 8) | int(payload[8])
+		isU2F := payload[4] == CTAPHID_MSG && val2b == U2F_SW_CONDITIONS_NOT_SATISFIED
+		isFIDO2 := payload[4] == CTAPHID_KEEPALIVE && val1b == STATUS_UPNEEDED
 
-		newMessage := notifier.U2F_OFF
+		// Cancel previous U2F_OFF timer
+		if u2fOffTimer != nil {
+			u2fOffTimer.Stop()
+		}
+
+		// If an unknown message is received, most probably YubiKey was touched.
+		// But it's possible that some intermediate pings are being sent.
+		// Wait just a tiny little bit more to see if no new U2F_ON messages arrive.
+		u2fOffTimerDuration := 200 * time.Millisecond
+
 		if isU2F || isFIDO2 {
-			newMessage = notifier.U2F_ON
+			// Signify U2F_ON if this is the first time we receive it
+			if lastMessage != notifier.U2F_ON {
+				notifiers.Range(func(k, v interface{}) bool {
+					v.(chan notifier.Message) <- notifier.U2F_ON
+					return true
+				})
+				lastMessage = notifier.U2F_ON
+			}
+
+			// Extend U2F_OFF timer duration because the last message was U2F_ON
+			u2fOffTimerDuration = 2 * time.Second
 		}
 
-		if newMessage != lastMessage {
-			notifiers.Range(func(k, v interface{}) bool {
-				v.(chan notifier.Message) <- newMessage
-				return true
-			})
-		}
-
-		lastMessage = newMessage
+		// Signify U2F_OFF if no new messages arrive soon
+		u2fOffTimer = time.AfterFunc(u2fOffTimerDuration, func() {
+			if lastMessage != notifier.U2F_OFF {
+				notifiers.Range(func(k, v interface{}) bool {
+					v.(chan notifier.Message) <- notifier.U2F_OFF
+					return true
+				})
+				lastMessage = notifier.U2F_OFF
+			}
+		})
 	}
 }

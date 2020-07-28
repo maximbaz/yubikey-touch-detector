38ccdbbc508cac517f6e37bd82937a3e71680c13
Nils Steinger <git@n-st.de>
2020-07-28 11:53:34 +0200

    Use libnotify bindings instead of calling external program

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/menefotto/go-libnotify v0.0.0-20160821232856-351dc16572b9
 	github.com/rjeczalik/notify v0.9.2
 	github.com/scylladb/go-set v1.0.2
 	github.com/sirupsen/logrus v1.4.2
--- notifier/libnotify.go
+++ notifier/libnotify.go
@@ -1,28 +1,37 @@
 package notifier
 
 import (
-	"os/exec"
 	"sync"
 
 	log "github.com/sirupsen/logrus"
+	libnotify "github.com/menefotto/go-libnotify"
 )
-
-func notifySend(text string) error {
-	return exec.Command("notify-send", text).Run()
-}
 
 // SetupLibnotifyNotifier configures a notifier to show all touch requests with libnotify
 func SetupLibnotifyNotifier(notifiers *sync.Map) {
 	touch := make(chan Message, 10)
 	notifiers.Store("notifier/libnotify", touch)
 
+	init_success := libnotify.Init("yubikey-touch-detector")
+	defer libnotify.UnInit()
+
+	if !init_success {
+		log.Error("Cannot initialize desktop notifications!")
+		return
+	}
+
+	notification := libnotify.NotificationNew("YubiKey is waiting for a touch", "", "")
+	if notification == nil {
+		log.Error("Cannot create desktop notification!")
+		return
+	}
+
 	for {
 		value := <-touch
 		if value == GPG_ON || value == U2F_ON || value == HMAC_ON {
-			err := notifySend("YubiKey is waiting for a touch")
-			if err != nil {
-				log.Error("Cannot send desktop notification: ", err)
-			}
+			// Error check (!= nil) not possible because menefotto/go-libnotify
+			// uses a custom wrapper instead of builtin 'error'
+			notification.Show()
 		}
 	}
 }

34b594002f108b061b5579d76cb2fac70e3a0f36
Maxim Baz <git@maximbaz.com>
2024-11-21 20:14:57 +0100

    Bump version

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 BIN := yubikey-touch-detector
-VERSION = 1.11.0
+VERSION = 1.12.0
 
 PREFIX ?= /usr
 LIB_DIR = $(DESTDIR)$(PREFIX)/lib
--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 	"github.com/maximbaz/yubikey-touch-detector/notifier"
 )
 
-const appVersion = "1.11.0"
+const appVersion = "1.12.0"
 
 func main() {
 	truthyValues := map[string]bool{"true": true, "yes": true, "1": true}
